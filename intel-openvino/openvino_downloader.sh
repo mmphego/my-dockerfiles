5e98c7edcf485050c47936699d9b6a9401a56b59
Mpho Mphego <mpho112@gmail.com>
2020-08-27 16:43:03 +0200

    revert to old version of openvino

--- intel-openvino/openvino_downloader.sh
+++ intel-openvino/openvino_downloader.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget -P /tmp "http://registrationcenter-download.intel.com/akdlm/IRC_NAS/16803/l_openvino_toolkit_p_${1}.tgz"
+wget -P /tmp "http://registrationcenter-download.intel.com/akdlm/irc_nas/16612/l_openvino_toolkit_p_${1}.tgz"
